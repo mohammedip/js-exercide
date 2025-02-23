8c4ad2921ed28b294de3ee72a445e6edf18f9411
Mohammed reida <reidamohammed3@gmail.com>
2025-02-23 13:22:38 +0100

    add more anserses

--- exercice1.js
+++ exercice1.js
@@ -44,6 +44,68 @@ for(let i=0;i<itCompanies.length;i++){
 console.log(string);
 console.log("------------------");
 
+let Companie1 = "cisco"
+let Companie2 = "IBM"
+for(let i=0;i<itCompanies.length;i++){
 
+    if(Companie2.toLocaleLowerCase()==itCompanies[i].toLocaleLowerCase()){
 
+        console.log(itCompanies[i]);
+        break;
+    }else if(Companie2.toLocaleLowerCase()!=itCompanies[i].toLocaleLowerCase()  && i<itCompanies.length-1){
+        continue;
+    }else{
+        console.log("Company is not found");
+    }
+    
+}
 
+console.log("------------------");
+
+let count=0;
+let CompanyWithMoreThenOneO = [];
+
+for(let i=0;i<itCompanies.length;i++){
+    // itCompanies[i].forEach(element => {
+    //     if(element=="o"){
+    //         count+=1;
+    //         if(count>1){
+    //             CompanyWithMoreThenOneO+=itCompanies[i];
+    //         }
+    //     }
+    // });
+    for(let j=0;j<itCompanies[i].length;j++){
+        if(itCompanies[i][j]=="o"){
+                    count+=1;
+                    if(count>1){
+                        CompanyWithMoreThenOneO.push(itCompanies[i]);
+                        count=0;
+                    }
+                }
+    }
+}
+console.log(CompanyWithMoreThenOneO);
+
+console.log("------------------");
+
+itCompanies.sort();
+console.log(itCompanies);
+
+console.log("------------------");
+
+itCompanies.reverse();
+console.log(itCompanies);
+
+console.log("------------------");
+
+let first3CompaniesSlacied= itCompanies.slice(0,3);
+console.log(itCompanies);
+console.log(first3CompaniesSlacied);
+
+console.log("------------------");
+
+let last3CompaniesSlacied= itCompanies.slice(-2);
+console.log(itCompanies);
+console.log(last3CompaniesSlacied);
+
+console.log("------------------");
